ee101ba210d18bd7a21aeeeda597336278f290ac
Md Mofazzal Hossain <mofazzal.hossain@widespace.com>
2014-12-24 19:14:32 +0600

    Segregate the router for different path

--- snacksbar.js
+++ snacksbar.js
@@ -1,38 +1,60 @@
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Meteor.startup(function() {
+        // code to run on server at startup
+    });
 }
 
 Menus = new Mongo.Collection('menu');
+AdminUser = new Mongo.Collection('admin_user');
+SelectedMenu = new Mongo.Collection('selected_menu');
 
+if (Meteor.isClient) {
 
-if(Meteor.isClient){
-  
-  Template.body.helpers({
-    menuItems : function(){
-      return Menus.find({});
+    Meteor.startup(function() {
+        Hooks.init();
+
+        // Meteor.Router.add({
+        //     '/': 'loginTemplate',
+        //     '/insertMenu': 'insertMenu',
+        //     '/showMenu': 'showMenu'
+        // });
+    });
+
+    Router.route('/', function() {
+        this.layout('ApplicationLayout');
+        this.render('LoginTemplate');
+    });
+
+    Template.LoginTemplate.events({
+        "submit .login": function(event) {
+
+            var userText = event.target.username.value;
+            var passwordText = event.target.password.value;
+
+            user = AdminUser.find({ 
+                username: userText,
+                password: passwordText
+            }).fetch();
+
+            if(user != null){
+                Session.set('userId', user[0]._id);
+                Session.set('role', user[0].role);
+                Router.go('insertMenu');                
+            }else{
+                alert("Invalid username and password");
+            }
+            return false;
+        }
+    });    
+
+    Hooks.onLoggedIn = function() {
+        Session.set('userId', Meteor.userId());
+        Router.go('insertMenu');
     }
-  });
 
-  Template.body.events({
-    "submit .add_menu": function (event) {
-      
-      var menu_name = event.target.menu_name.value;
-      var menu_value = event.target.menu_value.value;
-
-      Menus.insert({
-        name : menu_name,
-        value : menu_value,
-        createdAt : new Date()
-      });
-
-      event.target.menu_name.value = "";
-      event.target.menu_value.value = "";
-
-      return false;
+    Hooks.onLoggedOut = function(){
+        Session.keys = {};
+        Router.go('/');
     }
-  });
-
 
 }
